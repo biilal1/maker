c3f8f28ded72404757a5c67b61cedc2001eb5c4d
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-07-07 10:06:50 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["s_e_t"]
+OWNER_NAME = "teto"
+BOT_TOKEN = "6022370529:AAHM5anvuHDDhKN52ypmcoKTLWnWFEPVDDE"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/kkk8lI"
+GROUP = "https://t.me/PP_5U"
+VIDEO = "https://telegra.ph/file/d7036697fe29ed3ea52ce.jpg"
+LOGS = "teto"
+
